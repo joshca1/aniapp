1bd0199e74fd815613520b7086f5a16eab7e577e
jose <joseacm1989@gmail.com>
2018-03-07 23:03:53 -0400

    pagina de recarga y retira

--- src/components/GameComponents/GameCreditRecharge.vue
+++ src/components/GameComponents/GameCreditRecharge.vue
@@ -3,6 +3,7 @@
       <game-header></game-header>
       <section class="section">
         <h1 class="title has-text-centered">Recargas</h1>
+        <h2 class="title is-4 is-warning has-text-centered">Solo se aceptaran transferencias desde 10.000 hasta 300.000 bs</h2>
         <div class="tile is-ancestor">
           <div class="tile is-vertical is-8">
             <div class="tile is-parent">
@@ -31,16 +32,47 @@
                   <div class="field">
                     <label for="" class="label">Banco Cliente:</label>
                     <div class="select">
-                      <select name="banco_emisor" id="banco_emisor">
-                        <option value="Bco_vzla">Banco de Venezuela</option>
-                        <option value="Bco_vzla">Banco de Venezuela</option>
-                        <option value="Bco_vzla">Banco de Venezuela</option>
-                        <option value="Bco_vzla">Banco de Venezuela</option>
-                        <option value="Bco_vzla">Banco de Venezuela</option>
+                      <select name="banco">
+                        <option value="" disabled selected>Seleccione un Banco</option>
+                        <option value="0156">100% BANCO</option>
+                        <option value="0196">ABN AMRO BANK</option>
+                        <option value="0172">BANCAMIGA BANCO MICROFINANCIERO, C.A.</option>
+                        <option value="0171">BANCO ACTIVO BANCO COMERCIAL, C.A.</option>
+                        <option value="0166">BANCO AGRICOLA</option>
+                        <option value="0175">BANCO BICENTENARIO</option>
+                        <option value="0128">BANCO CARONI, C.A. BANCO UNIVERSAL</option>
+                        <option value="0164">BANCO DE DESARROLLO DEL MICROEMPRESARIO</option>
+                        <option value="0102">BANCO DE VENEZUELA S.A.I.C.A.</option>
+                        <option value="0114">BANCO DEL CARIBE C.A.</option>
+                        <option value="0149">BANCO DEL PUEBLO SOBERANO C.A.</option>
+                        <option value="0163">BANCO DEL TESORO</option>
+                        <option value="0176">BANCO ESPIRITO SANTO, S.A.</option>
+                        <option value="0115">BANCO EXTERIOR C.A.</option>
+                        <option value="0003">BANCO INDUSTRIAL DE VENEZUELA.</option>
+                        <option value="0173">BANCO INTERNACIONAL DE DESARROLLO, C.A.</option>
+                        <option value="0105">BANCO MERCANTIL C.A.</option>
+                        <option value="0191">BANCO NACIONAL DE CREDITO</option>
+                        <option value="0116">BANCO OCCIDENTAL DE DESCUENTO.</option>
+                        <option value="0138">BANCO PLAZA</option>
+                        <option value="0108">BANCO PROVINCIAL BBVA</option>
+                        <option value="0104">BANCO VENEZOLANO DE CREDITO S.A.</option>
+                        <option value="0168">BANCRECER S.A. BANCO DE DESARROLLO</option>
+                        <option value="0134">BANESCO BANCO UNIVERSAL</option>  
+                        <option value="0177">BANFANB</option>
+                        <option value="0146">BANGENTE</option>
+                        <option value="0174">BANPLUS BANCO COMERCIAL C.A</option>
+                        <option value="0190">CITIBANK.</option>
+                        <option value="0121">CORP BANCA.</option>
+                        <option value="0157">DELSUR BANCO UNIVERSAL</option>
+                        <option value="0151">FONDO COMUN</option>
+                        <option value="0601">INSTITUTO MUNICIPAL DE CR&#201;DITO POPULAR</option>
+                        <option value="0169">MIBANCO BANCO DE DESARROLLO, C.A.</option>
+                        <option value="0137">SOFITASA</option>
                       </select>
                     </div>
                   </div>
-                  <div class="field">
+                  <div class="box">
+                    <p class="subtitle">Banco Transferido</p>
                     <div class="control">
                       <label class="radio">
                         <input type="radio" name="question">
